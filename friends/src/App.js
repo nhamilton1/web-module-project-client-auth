2305871f0e45837023862be56da02bd94668c4e0
nhamilton1 <nickh3592@gmail.com>
2021-09-14 14:30:32 -0400

    adding components

---
+++ friends/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import HomePage from './components/HomePage';
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/logout">Logout</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/logout" component={Logout} />
+          <Route path="/login" component={Login} />
+          <Route exact path="/" component={HomePage} />    
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
