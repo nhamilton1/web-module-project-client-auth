9a524d5255d8dc3af46c795ffaf4062d08622074
nhamilton1 <nickh3592@gmail.com>
2021-09-14 17:08:01 -0400

    finished

--- friends/src/App.js
+++ friends/src/App.js
@@ -18,10 +18,10 @@ function App() {
             <Link to="/">Home</Link>
           </li>
           <li>
-            {isLoggedIn ? <Link to="/friends">New Friend</Link> : <div></div>}
+            <Link to="/login">Login</Link>
           </li>
           <li>
-            <Link to="/login">Login</Link>
+            {isLoggedIn ? <Link to="/friends">New Friend</Link> : <div></div>}
           </li>
           <li>
             {isLoggedIn ? <Link to="/logout">Logout</Link> : <div></div>}
